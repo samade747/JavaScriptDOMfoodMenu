346b4adabbf3598eae15b61ed963345308778151
samade747 <samad.e747@gmail.com>
2023-10-24 22:01:33 +0500

    5451121

---
+++ app.js
@@ -0,0 +1,15 @@
+// creating a array of object of food items
+
+const foodMenu = [
+    // each object represent a food item
+    {
+     itemimage: '',
+     itemName : 'Karai',
+     itemPrice: '1200 PKR',
+     itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+     itemCategory: Dinner,
+     
+    },
+
+    
+]
