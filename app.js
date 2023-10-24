267e4d117fa878facce8411ad2913ffe0044d445
samade747 <samad.e747@gmail.com>
2023-10-24 22:35:14 +0500

    125131535

--- app.js
+++ app.js
@@ -3,13 +3,45 @@
 const foodMenu = [
     // each object represent a food item
     {
-     itemimage: '',
+     itemimage: './images/karai.jpg',
      itemName : 'Karai',
      itemPrice: '1200 PKR',
      itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
-     itemCategory: Dinner,
+     itemCategory: 'Dinner',
      
     },
 
-    
+
+    {
+        itemimage: './images/',
+        itemName : 'omlait Paratah',
+        itemPrice: '180 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+        itemCategory: 'Breakfast',
+        
+       },
+
+
+       {
+        itemimage: './images/karai.jpg',
+        itemName : 'salad',
+        itemPrice: '150 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+        itemCategory: 'lunch',
+        
+       },
+
+       {
+        itemimage: './images/karai.jpg',
+        itemName : 'Chocolate Shake',
+        itemPrice: '180 PKR',
+        itemDescripition: 'lorem lreeandadjjdsajkjsaddas',
+        itemCategory: 'shakes',
+        
+       },
+
+
 ]
+// array mai category ke buttons
+const buttons = ['All', 'Breakfast', 'Lunch', 'shakes'];
+
