90446f401b1d46a9a2aa6b1d25835fb0693b18a9
samade747 <samad.e747@gmail.com>
2023-10-26 18:05:10 +0500

    2112

--- app.js
+++ app.js
@@ -78,6 +78,38 @@ if (filteredItems.length === 0){
     filteredItems = foodMenu;
 }
 
+filteredItems.forEach(function (item) {
+    for (let i = 0; i < 2; i++) {
+        const col = document.createElement('div');
+        col.classList.add('col-md-4');
+        col.classList.add('col-sm-6');
+        col.classList.add('mb-3');
+    
+        const div = document.createElement('div');
+        div.style.display = "flex";
+        div.style.justifyContent = "center";
+        div.style.alignItems = "center";
+
+        div.innerHTML = `
+            <div class="card" style="width: 18rem; border:1px solid lightgrey; border-radius:15px; box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset; -webkit-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset; -moz-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;">
+                <img class="card1img" style="border-radius:15px;" src="${item.itemimage}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-titleard1Name" style="color:rgb(252, 163, 9); text-transform:capitalize;">${item.itemName}</h5>
+                    <h6 class="card-title card1Price" style="font-size:large;">RS : <span style="color:rgb(252, 163, 9);">${item.itemPrice}</span></h6>
+                    <p class="card-text" style="color:black; font-size:medium;">${item.itemDescripition}</p>
+                </div>
+            </div>
+        `;
+
+        col.appendChild(div);
+        row.appendChild(col);
+    }
+});
+}
+// Finally, append the row to your container
+MenuContainer.appendChild(row);
+
+
 // Displaying the item
 // filteredItems.forEach(function (item){
 //   MenuContainer.innerHTML += `  
@@ -97,73 +129,74 @@ if (filteredItems.length === 0){
 //   </div>
 
 
-let cardContainer = document.querySelector('menu-items-display')
+// let row = document.createElement('div')
+// row.classList.add('row')
 
-let row = document.createElement('div')
-row.classList.add('row')
-
-filteredItems.forEach( function (item) {
+// filteredItems.forEach( function (item) {
+//   MenuContainer.innerHTML +=
   
+//   for (i = 0 ; i < 2 ; i++ ) {
+//       col =  document.createElement('div')
+//           col.classList.add('col-md-4')
+//           col.classList.add('col-sm-6')
+//           col.classList.add('mb-3')
+//   }
   
-  for (i = 0 ; i < 2 ; i++ ) {
-      col =  document.createElement('div')
-          col.classList.add('col-md-4')
-          col.classList.add('col-sm-6')
-          col.classList.add('mb-3')
-  }
-  MenuContainer.innerHTML += 
-  div = document.createElement('div')
-  div.style.display = "flex"
-  div.style.justifyContent = "center"
-  div.style.alignItems = "center"
+//   div = document.createElement('div')
+//   div.style.display = "flex"
+//   div.style.justifyContent = "center"
+//   div.style.alignItems = "center"
  
   
-  `<div 
-  class="card" 
-  style="width: 18rem; 
-  border:1px solid lightgrey;  
-  border-radius:15px; box-shadow: box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;
-  -webkit-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;
-  -moz-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;">
+//   `<div 
+//   class="card" 
+//   style="width: 18rem; 
+//   border:1px solid lightgrey;  
+//   border-radius:15px; box-shadow: box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;
+//   -webkit-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;
+//   -moz-box-shadow: 0px 0px 58px -24px rgba(0,0,0,0.65) inset;">
 
-  <img 
-  class="card1img" 
-  style="border-radius:15px;" 
-  src="${images[imageIndex]}" 
-  class="card-img-top">
+//   <img 
+//   class="card1img" 
+//   style="border-radius:15px;" 
+//   src="${images[imageIndex]}" 
+//   class="card-img-top">
 
-  <div 
-  class="card-body">
+//   <div 
+//   class="card-body">
 
-  <h5 
-  class="card-titleard1Name" 
-  style="color:rgb(252, 163, 9); 
-  text-transform:capitalize;">
-  ${menuCard.name}</h5>
+//   <h5 
+//   class="card-titleard1Name" 
+//   style="color:rgb(252, 163, 9); 
+//   text-transform:capitalize;">
+//   ${menuCard.name}</h5>
 
-  <h6 class="card-title 
-  card1Price" 
-  style="font-size:large;">
-  RS : 
-  <span 
-  style="color:rgb(252, 163, 9);">
-  ${menuCard.RS}</span></h6>
+//   <h6 class="card-title 
+//   card1Price" 
+//   style="font-size:large;">
+//   RS : 
+//   <span 
+//   style="color:rgb(252, 163, 9);">
+//   ${menuCard.RS}</span></h6>
 
-  <p 
-  class="card-text" 
-  style="color:black; 
-  font-size:medium;"> 
-  ${menuCard.text}</p>
+//   <p 
+//   class="card-text" 
+//   style="color:black; 
+//   font-size:medium;"> 
+//   ${menuCard.text}</p>
 
-  </div>`
+//   </div>`
 
-  col.appendChild(div)
-  row.appendChild(col)
-  cardContainer.appendChild(row)
-})
-});
-}
+//   col.appendChild(div)
+//   row.appendChild(col)
+//   cardContainer.appendChild(row)
+// })
+// });
 
-// Initial invocation of showItems to display all items
-showItems();
+
+// // Initial invocation of showItems to display all items
+// showItems();
+
+
+
 
